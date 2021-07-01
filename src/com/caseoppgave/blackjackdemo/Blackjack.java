efbe9f2c9554a79a6f51d8cd0fa3cf455fb0e5af
fuzzboii <54532695+fuzzboii@users.noreply.github.com>
2021-07-01 22:43:03 +0200

    Kontroll klassen Blackjack kan teste opprettelse av spillebunke

--- src/com/caseoppgave/blackjackdemo/Blackjack.java
+++ src/com/caseoppgave/blackjackdemo/Blackjack.java
@@ -2,6 +2,14 @@ package com.caseoppgave.blackjackdemo;
 
 public class Blackjack {
     public static void main(String[] args) {
-        
+
+        // Velkomst melding
+        System.out.println("Welcome to Blackjack");
+
+        //Oppretter spillebunken
+        Deck playingDeck = new Deck();
+        playingDeck.createFullDeck();
+
+        System.out.println((playingDeck));
     }
 }
