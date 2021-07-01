0c4dda68233ce2b5fd7fea5e01981e77a311f25a
fuzzboii <54532695+fuzzboii@users.noreply.github.com>
2021-07-01 21:54:36 +0200

    Suit, og value

--- src/com/caseoppgave/blackjackdemo/Card.java
+++ src/com/caseoppgave/blackjackdemo/Card.java
@@ -1,4 +1,7 @@
 package com.caseoppgave.blackjackdemo;
 
 public class Card {
+
+    private Suit suit;
+    private Value Value;
 }
--- src/com/caseoppgave/blackjackdemo/Value.java
+++ src/com/caseoppgave/blackjackdemo/Value.java
@@ -1,5 +1,5 @@
 package com.caseoppgave.blackjackdemo;
 
-public enum Values {
+public enum Value {
     TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 }
